7253c14ef326718c96788e3c5e8953c492aba10b
rioj7 <38918937+rioj7@users.noreply.github.com>
2021-11-15 00:53:36 +0100

    v0.1.0 First release

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# replace-on
-Visual Studio Code extension to replace selections/current-word based on rules
+Replace selected text or word under cursor when selection changes (immediate) or when calling a command (by key binding).
+
+> This extension looks a lot like [Replace Rules](https://marketplace.visualstudio.com/items?itemName=bhughes339.replacerules) by bhughes339.
+>
+> The difference is the possibility of immediate replacement and change of word under cursor (next version).
+
+The selected text (for a particular Multi Cursor) must match the search complete. It want to search for a partial match you have to add `(.*?)` to the start and end of the search string. If you don't need these strings in the replace string you can use `.*?` or `(?:.*?)`
+
+If a rule is an `immediate` rule it is evaluated the moment you Double Click or Select Word or another means of modify a selection.
+
+If you have Multi Cursors each selection is treated separately.
+
+The rules **not** marked as `immediate` are evaluated when you call the command `replace-on.selection-changed` from the command menu as **Replace On: Replace selection/word by defined rules** or with a key binding. In a key binding you can add an `args` property. The `args` property has the same format as the `replace-on.selection-changed` configuration setting.
+
+# Configuration
+
+You specify the search/replace strings in the configuration setting `replace-on.selection-changed`.
+
+The properties of `replace-on.selection-changed` are:
+
+* `"all"`: an object with search/replace rules that are applied to all languageIds
+*  <code>"<em>languageId</em>"</code>: an object with search/replace rules that are applied only to files with the given languageId
+
+The rules you add for a specific languageId replace rules from the `all` section with the same search string. Be aware that the settings from User, Workspace and Folder are merged.
+
+## Object with search/replace rules
+
+The object with search/replace rules has the following format:
+
+* the key is the string to search (literal or regular expression)
+* the value is an object with properties:
+
+    * `"replace"`: the replacement string if the key matches the selection, if it is a regex search/replace this string can contain capture group references, like `"$1"`
+    * `"immediate"`: a boolean that signals that this rule can be applied the moment the selection changes (default: `false`)
+    * `"literal"`: a boolean that signals that the search should be for a literal string, when `false` the search is using Regex (default: `false`)
+    * `"flags"`: a string of flags used in the regex search. Only `"i"` makes sense because the whole search string must match the selection (default: `""`)
+
+## Example
+
+```
+  "replace-on.selection-changed": {
+    "all": {
+      "[ABC]": {
+        "replace": "[MNP]",
+        "literal": true,
+        "immediate": true
+      },
+      "A(D+)C": {
+        "replace": "M$1P",
+        "flags": "i",
+        "immediate": true
+      }
+    },
+    "javascript": {
+      "foo": {
+        "replace": "Bar",
+        "flags": "i"
+      }
+    }
+  }
+```
---
+++ replace-on.js
@@ -0,0 +1,97 @@
+const vscode = require('vscode');
+
+const getProperty = (obj, prop, deflt) => { return obj.hasOwnProperty(prop) ? obj[prop] : deflt; };
+const isString = obj => typeof obj === 'string';
+const isObject = obj => typeof obj === 'object';
+const isArray = obj => Array.isArray(obj);
+const isUri = obj => isObject(obj) && obj.hasOwnProperty('scheme');
+
+class ReplaceProvider {
+  constructor() {
+    this.replaceMap = {};
+  }
+  /** @param {vscode.TextEditor} editor */
+  updateEditorReplaceMap(editor) {
+    this.replaceMap = {};
+    if (!editor) { return; }
+    var document = editor.document;
+    if (!document) { return; }
+    var workspaceFolder = vscode.workspace.getWorkspaceFolder(document.uri);
+    var config = vscode.workspace.getConfiguration('replace-on.selection-changed', workspaceFolder ? workspaceFolder.uri : null);
+    this.replaceMap = this.parseConfiguration(config, document.languageId);
+  }
+  parseConfiguration(config, languageId) {
+    let replaceMap = {};
+    this.updateReplaceMap(replaceMap, config['all']);
+    this.updateReplaceMap(replaceMap, config[languageId]);
+    return replaceMap;
+  }
+  updateReplaceMap(replaceMap, replaceJSON) {
+    for (const search in replaceJSON) {
+      if (!replaceJSON.hasOwnProperty(search)) { continue; }
+      const properties = replaceJSON[search];
+      let rule = {};
+      rule.replace = getProperty(properties, 'replace');
+      if (rule.replace === undefined) { continue; }
+      rule.immediate = getProperty(properties, 'immediate');
+      rule.literal = getProperty(properties, 'literal');
+      if (rule.literal) {
+        rule.search = search;
+      } else {
+        let flags = getProperty(properties, 'flags');
+        rule.search = new RegExp(search, flags);
+      }
+      replaceMap[search] = rule;
+    }
+  }
+  /** @param {vscode.TextEditor} editor @param {vscode.TextEditorEdit} edit */
+  selectionChanged(editor, edit, args, byCommand) {
+    let replaceMap = this.replaceMap;
+    if (args) {
+      replaceMap = this.parseConfiguration(args, editor.document.languageId);
+    }
+    for (const selection of editor.selections) {
+      if (selection.isEmpty) { continue; }
+      let text = editor.document.getText(new vscode.Range(selection.start, selection.end));
+      for (const search in replaceMap) {
+        if (!replaceMap.hasOwnProperty(search)) { continue; }
+        const rule = replaceMap[search];
+        if (Boolean(rule.immediate) !== Boolean(byCommand) ) {  // logical XOR
+          if (rule.literal) {
+            if (rule.search !== text) { continue; }
+            text = rule.replace;
+          } else {
+            let match = text.match(rule.search);
+            if (match === null || match.index !== 0 || match[0] !== text) { continue; }
+            text = text.replace(rule.search, rule.replace);
+          }
+          edit.replace(selection, text);
+        }
+      }
+    }
+  }
+}
+
+function activate(context) {
+
+  const replaceProvider = new ReplaceProvider();
+
+  context.subscriptions.push(vscode.commands.registerTextEditorCommand('replace-on.selection-changed', (editor, edit, args) => replaceProvider.selectionChanged(editor, edit, args, true)));
+
+  vscode.window.onDidChangeTextEditorSelection(changeEvent => { changeEvent.textEditor.edit( editBuilder => replaceProvider.selectionChanged(changeEvent.textEditor, editBuilder) ); },
+        null, context.subscriptions);
+
+  const onChangeActiveTextEditor = () => {
+    replaceProvider.updateEditorReplaceMap(vscode.window.activeTextEditor);
+  };
+  vscode.window.onDidChangeActiveTextEditor(onChangeActiveTextEditor, null, context.subscriptions);
+  onChangeActiveTextEditor();
+}
+
+function deactivate() {
+}
+
+module.exports = {
+  activate,
+  deactivate
+}
